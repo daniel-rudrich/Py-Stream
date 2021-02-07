e3f9b1b670ae55c4c8924944ca43c7bceac5235b
Daniel Rudrich <daniel.rudrich@fau.de>
2021-02-07 12:09:12 +0100

    added image_upload endpoint

--- src/streamdeck/urls.py
+++ src/streamdeck/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('streamdecks/<int:id>/folders', views.streamdeck_folders),
     path('streamdecks/<int:deck_id>/folders/<int:id>', views.streamdeck_folder),
     path('key/<int:id>', views.key_detail),
+    path('key/<int:id>/image_upload', views.key_image_upload),
     path('key/<int:id>/command', views.command_create),
     path('key/<int:key_id>/command/<int:id>', views.command_detail),
     path('key/<int:key_id>/folder', views.create_folder),
