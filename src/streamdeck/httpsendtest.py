8127fb40cf5fcbb2afc3700d1b122276378c0b83
Daniel Rudrich <danielrudrich@yahoo.de>
2021-06-08 16:23:42 +0200

    added flake8 config

--- src/streamdeck/httpsendtest.py
+++ src/streamdeck/httpsendtest.py
@@ -1,6 +1,7 @@
 import requests
 files = {'image_source': open(
-    '/home/daniel/Projekt/python-elgato-streamdeck/src/Assets/RGB_color_space_animated_view.gif', 'rb')}
+    '/home/daniel/Projekt/python-elgato-streamdeck/src/Assets/' /
+    'RGB_color_space_animated_view.gif', 'rb')}
 r = requests.put('http://127.0.0.1:8000/key/17/image_upload',
                  data={}, files=files)
 print(r.status_code)
